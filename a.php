9013f0bbc3dfef2d839214f5a0400a663ee7f6d4
DM <769245396@qq.com>
2017-09-01 18:36:48 +0800

    --

--- a.php
+++ a.php
@@ -4,8 +4,8 @@
 // 第三行
 // 第四行
 
-// 测试 分支merge 当前在master分支 从dev合并到master
+// 测试 分支merge 当前在master分支 从dev合并到master git merge dev
 
-// 我要把这行合并到dev分支 当前在dev分支 从master合并到dev
+// 我要把这行合并到dev分支 当前在dev分支 从master合并到dev git merge master
 
-// 开始测试rebase 当前在dev分支
+// 开始测试rebase 当前在dev分支 正确结果:当前在master 执行 git rebase dev 后 dev上的内容才到master上
